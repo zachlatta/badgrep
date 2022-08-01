d6422b1fa4d9de8a34c496f9cc17f65306efacbb
Zach Latta <zach@zachlatta.com>
2022-08-01 02:35:42 +0000

    Recursively search files

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,32 @@ fn main() {
 
     let search = &args[1];
 
-    let paths = fs::read_dir("./").unwrap();
+    search_dir_recursive("./", search)
+}
+
+fn search_dir_recursive(dir_to_search: &str, search: &str) {
+    let paths = fs::read_dir(dir_to_search).unwrap();
 
     for path in paths {
         let unwrapped_path = path.unwrap().path();
 
-        let md = fs::metadata(&unwrapped_path);
+        let md = fs::metadata(&unwrapped_path).unwrap();
 
-        if md.unwrap().is_file() {
-            let contents = fs::read_to_string(&unwrapped_path).unwrap();
+        if md.is_file() {
+            let contents = fs::read_to_string(&unwrapped_path);
+
+            let contents = match contents {
+                Ok(contents) => contents,
+                Err(_error) => continue
+            };
 
             if contents.contains(search) {
-                println!("{} contains {}", unwrapped_path.file_name().unwrap().to_str().unwrap(), search);
+                println!("{} contains {}", unwrapped_path.to_str().unwrap(), search);
             }
+        }
+
+        if md.is_dir() {
+            search_dir_recursive(unwrapped_path.to_str().unwrap(), search)
         }
     }
 }
